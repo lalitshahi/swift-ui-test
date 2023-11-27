848585070a38485f5744d182083799fdc32b46a9
Lalit Shahi <shahilalit002@gmail.com>
2023-11-27 14:17:58 +0530

    Initial Commit

---
+++ Swift-ui-weather/Swift_ui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ui_weatherApp.swift
+//  Swift-ui-weather
+//
+//  Created by Lalit Shahi on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
